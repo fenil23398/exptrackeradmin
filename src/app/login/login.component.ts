97812423b0493f088ad9ce9749e71c4fd1d9dc6a
shivanidesai14 <shivanidesai402@gmail.com>
2018-04-29 11:56:57 +0530

    login & forgrt pass

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,53 @@
 import { Component, OnInit } from '@angular/core';
-
+import { LogindbService } from './logindb.service';
+import { Router } from '@angular/router';
+import { NgForm,FormControl } from '@angular/forms';
+import { UserServiceService } from '../homepage/user-service.service';
+import { Users } from '../homepage/user_model';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  email:string;
+  pass:string;
+  id:number=0;
+  img:string;
+  dpass:number;
+  name:string;
+  mobno:string;
+  eid:string;
+  msg:string;
+  constructor(public _data:LogindbService,public _r:Router,public _datause:UserServiceService) { }
 
   ngOnInit() {
   }
-
-}
+  onlogin()
+  {
+    let item=new Users(this.id,this.email,this.name,this.mobno,this.img,this.pass,this.dpass);
+    this._data.getUserByLogin(item).subscribe(
+      (data)=>{
+        if(data=='')
+        {
+          alert('Email Id and Password is Incorrect');
+        }
+        else{
+          
+          localStorage.setItem('name',this.email);
+          localStorage.setItem('pass',this.pass);
+          this.eid=localStorage.getItem('name');      
+          this._r.navigate(['/home']);
+        
+        }
+      },
+      function(e)
+      {
+        alert(e);
+      },
+      function()
+      {
+      }
+    );
+  }
+  }
--- src/app/login/logindb.service.ts
+++ src/app/login/logindb.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@angular/core';
-
+import {HttpClient,HttpHeaders} from "@angular/common/http";
 @Injectable()
 export class LogindbService {
-
-  constructor() { }
+  public url:string='http://exptracker1.herokuapp.com/login/';
+  public url1:string='http://exptracker1.herokuapp.com/forgetpass/';
+  constructor(public http: HttpClient) { }
+  getUserByLogin(item)
+  {
+    let body=JSON.stringify(item);
+    return this.http.post(this.url,body,{headers:new HttpHeaders().set('Content-Type','application/json')});
+  }
+  sendMail(item)
+  {
+    let body=JSON.stringify(item);
+    return this.http.post(this.url1,body,{headers:new HttpHeaders().set('Content-Type','application/json')});
+  }
 
 }
